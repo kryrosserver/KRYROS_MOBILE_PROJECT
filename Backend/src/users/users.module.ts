e3515b30dc12f858adf8a9ef29a27eac8c94df7d
kryrosserver <kryrosserver@gmail.com>
2026-06-01 14:32:02 +0100

    feat: import CloudinaryModule into UsersModule

--- Backend/src/users/users.module.ts
+++ Backend/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryModule } from '../common/cloudinary/cloudinary.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [CloudinaryModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
