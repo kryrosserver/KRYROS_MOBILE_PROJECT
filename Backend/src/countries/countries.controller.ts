5990f989c0aba0e5af129fbcafe441bfa4b81a6a
kryrosserver <kryrosserver@gmail.com>
2026-06-01 18:33:34 +0100

    fix: correct broken UseInterceptors import in countries.controller.ts

--- Backend/src/countries/countries.controller.ts
+++ Backend/src/countries/countries.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } , UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { CountriesService } from './countries.service';
